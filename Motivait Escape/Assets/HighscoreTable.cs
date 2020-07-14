16d134b1214d27a4d2da340a2e9192dc38389668
Corey Veitch-McAllister <56450139+CoreyVM@users.noreply.github.com>
2020-07-14 16:39:19 +0100

    Finished implementing highscore system

--- Motivait Escape/Assets/HighscoreTable.cs
+++ Motivait Escape/Assets/HighscoreTable.cs
@@ -8,26 +8,27 @@ public class HighscoreTable : MonoBehaviour
     public Text playerTime;
     public Text highscoreText;
     public Timer gameTimer;
+    public Text NewScoreSetText;
 
     private string highscoreMinutes = "05";
     private string highscoreSeconds = "30";
     private int playerMinutes;
-    private int playerSeconds;
+    private float playerSeconds;
 
     private void Start()
     {
         string value = PlayerPrefs.GetString("HighscoreMinutes", highscoreMinutes) +  " : " + PlayerPrefs.GetString("HighscoreSeconds", highscoreSeconds);
         GetPlayerFinalTime();
         highscoreText.text = value;
-       
+
     }
 
     private void GetPlayerFinalTime()
     {
-        float seconds = float.Parse(gameTimer.GetSeconds());
-        int minutes = int.Parse(gameTimer.GetMinutes());
-        playerTime.text = minutes.ToString() + " : " + seconds.ToString();
-   //     CheckForNewHighScore();
+       playerSeconds = float.Parse(gameTimer.GetSeconds());
+        playerMinutes = int.Parse(gameTimer.GetMinutes());
+       playerTime.text = playerMinutes.ToString() + " : " + playerSeconds.ToString();
+        CheckForNewHighScore();
     }
 
     void CheckForNewHighScore()
@@ -35,17 +36,41 @@ public class HighscoreTable : MonoBehaviour
         float Highseconds = float.Parse(PlayerPrefs.GetString("HighscoreSeconds", highscoreSeconds));
         int Highminutes = int.Parse(PlayerPrefs.GetString("HighscoreMinutes", highscoreMinutes));
 
-
-
-        if (Highminutes >= playerMinutes)
+        if (playerMinutes > Highminutes)
+            SetNewHighScore();
+        
+        else if(playerMinutes == Highminutes)
         {
-            if (Highseconds > playerSeconds)
+            if (playerSeconds > Highseconds)
             {
-                Debug.Log("The highscore will stay the samew");
+                SetNewHighScore();
             }
         }
       
   
+    }
+
+    void SetNewHighScore()
+    {
+        PlayerPrefs.SetString("HighscoreSeconds", playerSeconds.ToString());
+        PlayerPrefs.SetString("HighscoreMinutes", playerMinutes.ToString());
+        highscoreMinutes = playerMinutes.ToString();
+        highscoreSeconds = playerSeconds.ToString();
+        highscoreText.text = highscoreMinutes + " : " + highscoreSeconds;
+        StartCoroutine(ShowWinText());
+    }
+
+    IEnumerator ShowWinText()
+    {
+        NewScoreSetText.enabled = true;
+        yield return new WaitForSeconds(5);
+        NewScoreSetText.enabled = false;
+    }
+
+    private void RevertHighScore()
+    {
+        PlayerPrefs.DeleteKey("HighscoreSeconds");
+        PlayerPrefs.DeleteKey("HighscoreMinutes");
     }
 }
 
