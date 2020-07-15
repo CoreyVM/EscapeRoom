bcfb10e34c3e24d727e2c0a801d131a344d1c53f
Corey Veitch-McAllister <56450139+CoreyVM@users.noreply.github.com>
2020-07-15 14:51:10 +0100

    Added Basic Option Screen (No logic implemented all art)

---
+++ Motivait Escape/Assets/SettingsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void SetFullscreen(bool value)
+    {
+  
+    }
+
+    public void SetQuality(int qualityIndex) //Use the index to set the game quality
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+  
+
+}
